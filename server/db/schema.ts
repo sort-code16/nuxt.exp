261da4609c25de6407233fee98c29ff4c8f60520
sort.code16 <sort.code16@gmail.com>
2026-09-10 01:11:47 +0300

    feat: add email field to users table and update related schema

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -8,5 +8,6 @@ export const boardsTable = sqliteTable('boards', {
 export const usersTable = sqliteTable('users', {
     id: int().primaryKey({ autoIncrement: true }),
     username: text().notNull(),
+    email: text().notNull().unique(),
     password: text().notNull(),
 });
